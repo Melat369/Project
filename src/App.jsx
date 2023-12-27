4ac4a6dbe394632629055ed5aa62f8640f869d2b
Melat t <melattesfaye16@gmail.com>
2023-12-27 20:21:38 +0300

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './Components/NavBar/Navbar'
+import Search from './Components/Search/Search'
+import Jobs from './Components/JobDiv/Jobs'
+import Value from './Components/ValueDiv/Value'
+import Footer from './Components/FooterDiv/Footer'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar /> 
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
     </div>
   )
 }
